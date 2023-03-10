4a1a70daacd6c99da627a751c8d1b89fbdf64c78
Glindley23 <graham.lindley23@gmail.com>
2023-03-09 22:07:28 -0500

    finished post route for login

--- app.js
+++ app.js
@@ -21,5 +21,23 @@ app.get('/', (req, res) => {
 app.post('/login', (req,res) => {
     let username = req.body.username;
     let password = req.body.password;
-    
+    let query = "SELECT title FROM user where username = '" + username + "' and password = '" + password + "'";
+
+    console.log("username: " + username);
+	console.log("password: " + password);
+	console.log('query: ' + query);
+
+    db.get(query, function (err, row) {
+
+		if (err) {
+			console.log('ERROR', err);
+			res.redirect("/index.html#error");
+		} else if (!row) {
+			res.redirect("/index.html#unauthorized");
+		} else {
+			res.send('Hello <b>' + row.title + '!</b><br /> This file contains all your secret data: <br /><br /> SECRETS <br /><br /> MORE SECRETS <br /><br /> <a href="/index.html">Go back to login</a>');
+		}
+    })
 })
+
+app.listen(3000)
